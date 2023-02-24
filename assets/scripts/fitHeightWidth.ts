53d89fbea470e50d1a6c8af769a3a32c94da0703
ChicMic Technologies <chicmictechnologies@cpe-192-180-0-107.kya.res.rr.com>
2023-02-24 16:59:33 +0530

    Multi Resolution testing on Creator only

--- assets/scripts/fitHeightWidth.ts
+++ assets/scripts/fitHeightWidth.ts
@@ -1,20 +1,67 @@
-import { _decorator, Component, Node, UITransform, Widget, math, tween, Vec3 } from 'cc';
+import { _decorator, Component, Node, UITransform, Widget, math, tween, Vec3, Enum } from 'cc';
 const { ccclass, property } = _decorator;
+
+enum select_Type {
+    None = 0,
+    fit_Without_Crop = 1,
+    fit_With_Crop = 2
+};
+
 
 @ccclass('fitHeightWidth')
 export class fitHeightWidth extends Component {
 
+    // screenN: Node = this.node.getChildByName("screenDisplay");
+    // display: Node = this.node.getChildByName("designDisplay");
+
+
+    @property({ type: Enum(select_Type) })
+    selectType: select_Type = select_Type.None;
+
     @property(Node)
     square: Node | null = null;
+
     @property(Node)
     rectangle: Node | null = null;
 
+    @property(Node)
+    get frame(): Node {
+        console.log("This is frame's get()!");
+        return this.rectangle;
+    }
+    set frame(Value: Node) {
+        console.log("This is frame's set()!");
+        this.rectangle = Value;
+        this.setHeiWid();
+    }
+
+
     onLoad() {
+        // if (this.screenN && this.display) {
+        //     let idxOfScrn = this.screenN.getSiblingIndex();
+        //     this.screenN.setSiblingIndex(this.display.getSiblingIndex());
+        //     this.display.setSiblingIndex(idxOfScrn);
+        //     console.log("screen Idx: " + this.screenN.getSiblingIndex());
+        //     console.log("display Idx: " + this.display.getSiblingIndex());
+        // }
 
 
     }
 
     start() {
+
+
+
+
+
+    }
+
+    update(deltaTime: number) {
+
+    }
+
+
+    setHeiWid() {
         let squareDim = {
             "width": this.square.getComponent(UITransform).width,
             "height": this.square.getComponent(UITransform).height,
@@ -27,19 +74,37 @@ export class fitHeightWidth extends Component {
         let aspectRatioSq = squareDim["width"] / squareDim["height"];
         let aspectRatioRect = rectDim["width"] / rectDim["height"];
 
-        // this.fitHeightWidthWithCrop(aspectRatioSq, aspectRatioRect, squareDim, rectDim);
-        this.fitHeightWidth(squareDim, rectDim);
-
-        
-       
-
+        switch (this.selectType) {
+            case select_Type.fit_With_Crop: {
+                this.fitHeightWidthWithCrop(aspectRatioSq, aspectRatioRect, squareDim, rectDim);
+                break;
+            }
+            case select_Type.fit_Without_Crop: {
+                this.fitHeightWidth(squareDim, rectDim);
+                break;
+            }
+        }
     }
 
-    update(deltaTime: number) {
 
-    }
+
+
+
+
 
     fitHeightWidthWithCrop(aspS: number, aspR: number, sq, rect) {
+
+
+        let screen = this.node.getChildByName("screenDisplay");
+        let display = this.node.getChildByName("designDisplay");
+        console.log("child: " + this.node.children);
+        if (screen && display) {
+            let idxOfScrn = screen.getSiblingIndex();
+            screen.setSiblingIndex(display.getSiblingIndex());
+            display.setSiblingIndex(idxOfScrn);
+            console.log("screen Idx: " + screen.getSiblingIndex());
+            console.log("display Idx: " + display.getSiblingIndex());
+        }
 
         if (aspS <= aspR) {
             // change height
@@ -58,32 +123,32 @@ export class fitHeightWidth extends Component {
 
 
 
-    fitHeightWidth(squareDim, rectDim){
-        
-        if(rectDim["width"]<rectDim["height"]){
+    fitHeightWidth(squareDim, rectDim) {
+
+        if (rectDim["width"] < rectDim["height"]) {
             // Height changes
-            let ratio = squareDim["height"]/rectDim["height"];
+            let ratio = squareDim["height"] / rectDim["height"];
             let scale = this.rectangle.getScale();
             this.rectangle.setScale(scale.x, scale.y * ratio, 1);
         }
-        else if(rectDim["width"]>rectDim["height"]){
-            let ratio = squareDim["width"]/rectDim["width"];
+        else if (rectDim["width"] > rectDim["height"]) {
+            let ratio = squareDim["width"] / rectDim["width"];
             let scale = this.rectangle.getScale();
             this.rectangle.setScale(scale.x * ratio, scale.y, 1);
         }
-        else{
+        else {
             let scale = this.rectangle.getScale();
-            let ratioW = squareDim["width"]/rectDim["width"];
-            let ratioH = squareDim["height"]/rectDim["height"];
+            let ratioW = squareDim["width"] / rectDim["width"];
+            let ratioH = squareDim["height"] / rectDim["height"];
             this.rectangle.setScale(scale.x * ratioW, scale.y * ratioH, 1);
 
-            
+
         }
         // let pos = this.rectangle.getPosition();
         // tween(this.rectangle)
         //     .to(3, {position: new Vec3(pos.x, pos.y + 100, pos.z)})
         //     .start();
-        
+
 
     }
 }
